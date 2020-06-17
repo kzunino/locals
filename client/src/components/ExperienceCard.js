096d6dfb5fe55a33928a1f382cc48044fd84e8e2
Kyle <kzunino@gmail.com>
2020-06-16 17:42:11 -0700

    scroll buttons function and touch scroll function

--- client/src/components/ExperienceCard.js
+++ client/src/components/ExperienceCard.js
@@ -5,19 +5,17 @@ import Portrait from '../img/portrait.jpg';
 
 function ExperienceCard() {
   return (
-    <div>
-      <Card className='experience-card'>
-        <Card.Img variant='top' src={SampleImg} />
-        <img src={Portrait} alt='' className='experience-user-avatar' />
-        <Card.Body className='pt-0 pb-0'>
-          <p className='experience-subheading'>
-            Enjoy Tahoe with <span className='primary-color'>Tom</span>
-          </p>
-          <Card.Title className='card-title'>Hiking Mt.Tallac</Card.Title>
-          <p>$20 - 2 hours - Hiking</p>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card className='experience-card'>
+      <Card.Img variant='top' src={SampleImg} />
+      <img src={Portrait} alt='' className='experience-user-avatar' />
+      <Card.Body className='pt-0 pb-0'>
+        <p className='experience-subheading'>
+          Enjoy Tahoe with <span className='primary-color'>Tom</span>
+        </p>
+        <Card.Title className='card-title'>Hiking Mt.Tallac</Card.Title>
+        <p>$20 - 2 hours - Hiking</p>
+      </Card.Body>
+    </Card>
   );
 }
 
--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ExperienceCard from './ExperienceCard';
 
 function Slider() {
+  const ref = useRef(null);
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
+
   return (
     <div className='cards-slider'>
-      <div className='cards-slider-wrapper'>
+      <div className='cards-slider-wrapper' ref={ref}>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
       </div>
+      <button
+        className='scroll-left'
+        type='button'
+        onClick={() => scroll(-200)}
+      >
+        &lang;
+      </button>
+      <button
+        className='scroll-right'
+        type='button'
+        onClick={() => scroll(200)}
+      >
+        &rang;
+      </button>
     </div>
   );
 }
