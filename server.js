f19d36468ebb5c4369511cc9cc388d53fe225aa2
Kyle <kzunino@gmail.com>
2020-07-17 15:05:06 -0700

    trying to fix errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 const upload = require('express-fileupload');
 app.use(upload({useTempFiles: true}));
