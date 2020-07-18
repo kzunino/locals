d2987403d7f0d5e4bc9633ffe4cadb987a3ba45d
Kyle <kzunino@gmail.com>
2020-07-17 21:33:16 -0700

    fixing heroku error

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-//change to URI, make folder public and put URI into default.json
+// change to URI, make folder public and put URI into default.json
 require('dotenv').config();
 
 //PG_HOST = 127.0.0.1
@@ -13,18 +13,14 @@ module.exports = {
     port: process.env.PG_PORT,
     operatorsAliases: 0,
   },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    operatorsAliases: 0,
-  },
   production: {
-    //connectionString: process.env.DATABASE_URL,
-    use_env_variable: process.env.DATABASE_URL,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
     dialect: 'postgres',
+    port: process.env.PG_PORT,
+    use_env_variable: process.env.DATABASE_URL,
     dialectOptions: {
       ssl: true,
     },
