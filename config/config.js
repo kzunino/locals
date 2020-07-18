a7152ad2229fbc77435f0e6ed6b199cbd688dc19
Kyle <kzunino@gmail.com>
2020-07-17 21:48:20 -0700

    trying to fix heroku error again

--- config/config.js
+++ config/config.js
@@ -14,13 +14,9 @@ module.exports = {
     operatorsAliases: 0,
   },
   production: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    host: process.env.PG_HOST,
     dialect: 'postgres',
-    port: process.env.PG_PORT,
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable:
+      'postgres://bjvqvqovwpyqob:f88278fbc6ffef452220d6a2d318c8c798254a9ea48a5615462e91e613fd7a2b@ec2-34-236-215-156.compute-1.amazonaws.com:5432/d498l1h5c5rcbu',
     dialectOptions: {
       ssl: true,
     },
