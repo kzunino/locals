813c072b229d22bb9be42637c89734f81897274f
Kyle <kzunino@gmail.com>
2020-07-19 19:50:19 -0700

    took off uri

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 //PG_HOST = 127.0.0.1
 
-process.env.DATABASE_URL =
-  'postgres://wivdzorewkoeux:fecba625a2241b5a907d977a89e9067122c9d37dd8c0cd2bd2da093a00d8ccca@ec2-34-192-173-173.compute-1.amazonaws.com:5432/dgclfg4j2fv4r';
 console.log(process.env.DATABASE_URL);
 
 module.exports = {
